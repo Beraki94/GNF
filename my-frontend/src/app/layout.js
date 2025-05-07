714023384075ee5c7acd9cbedb95220cb3875ecf
Beraki Digital <berakidigital@gmail.com>
2025-05-07 19:27:53 +0100

    seo1

--- my-frontend/src/app/layout.js
+++ my-frontend/src/app/layout.js
@@ -4,22 +4,28 @@ import { albertSans, merriweather, poppins } from './font';
 import ClientLayout from './clientLayout';
 import AppWrapper from '@/components/appWrapper/AppWrapper';
 
-
 export const metadata = {
-  title: 'Your App',
-  description: 'A great app using Next.js and Google Fonts',
+  title: 'GodLight Nigeria Foundation',
+  description: 'Empowering communities and transforming lives through impactful initiatives in Nigeria.',
+  keywords: ['GodLight Nigeria', 'NGO in Nigeria', 'Charity Foundation', 'Community Empowerment'],
+  metadataBase: new URL('https://www.godlightnigeria.com'), // change to your actual domain
+  alternates: {
+    canonical: '/',
+  },
 };
 
 export default function RootLayout({ children }) {
   const fonts = `${albertSans.variable} ${merriweather.variable} ${poppins.variable}`;
 
   return (
-    <html lang="en">
-      <AppWrapper>
-        <ClientLayout fonts={fonts}>
-        {children}
-        </ClientLayout>
-      </AppWrapper>
+    <html lang="en" className={fonts}>
+      <body>
+        <AppWrapper>
+          <ClientLayout>
+            {children}
+          </ClientLayout>
+        </AppWrapper>
+      </body>
     </html>
   );
 }
